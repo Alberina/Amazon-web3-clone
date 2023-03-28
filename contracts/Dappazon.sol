fe3c71adc793eea313a1a3187db0c4c4efdc2b7f
Alberina <44498537+Alberina@users.noreply.github.com>
2023-03-28 20:36:06 +0200

    define item obj and add list function

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -4,7 +4,29 @@ pragma solidity ^0.8.9;
 contract Dappazon {
     address public owner;
 
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
     constructor(){
         owner = msg.sender;
+    }
+
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public {
+        
     }
 }
