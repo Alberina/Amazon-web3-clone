ff535f5b7a4a779c7d9259391d82747f9ae2710d
Alberina <44498537+Alberina@users.noreply.github.com>
2023-03-28 19:18:03 +0200

    set the owner address

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -7,14 +7,22 @@ const tokens = (n) => {
 
 describe("Dappazon", () => {
   let dappazon;
+  let deployer, buyer;
 
   beforeEach(async ()=>{
+    //set up acc
+
+    [deployer, buyer] = await ethers.getSigners();
     //deploy contract
     const DAPPAZON  = await ethers.getContractFactory("Dappazon");
     dappazon = await DAPPAZON.deploy();
   })
 
   describe("Deployment", () => {
+    it('Set the owner', async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    })
+
     it('has a name', async () => {
       const name = await dappazon.name();
       expect(name).to.equal("Dappazon");
