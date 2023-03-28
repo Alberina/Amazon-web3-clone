5c58942f0602048a585f1abc23268eaa076e8135
Alberina <44498537+Alberina@users.noreply.github.com>
2023-03-28 18:21:51 +0200

    smoke test

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,14 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappazon", () => {
-
+  it('has a name', async () => {
+    const DAPPAZON  = await ethers.getContractFactory("Dappazon");
+    dappazon = await DAPPAZON.deploy();
+    expect(await dappazon.name()).to.equal("Dappazon");
+  })
 })
